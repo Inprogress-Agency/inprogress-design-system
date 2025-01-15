ae03d21fd13af8ab308aad10f670994cfaf8c566
Paul Thiberville <paul.thiberville@live.fr>
2025-01-15 18:16:00 +0100

    gestion des namespaces et import des svg

--- .storybook/stories/Button.stories.tsx
+++ .storybook/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Button } from '../../src'
+import { Button } from '../../src/buttons'
 
 const meta: Meta<typeof Button> = {
   title: 'Button',
