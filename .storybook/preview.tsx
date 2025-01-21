02e9426797ddea7feb7651c2764268828b680d4d
Paul Thiberville <paul.thiberville@live.fr>
2025-01-21 14:02:36 +0100

    PB-1673 Phone mockup

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,56 +1,39 @@
-import React, { ComponentType, ReactNode } from 'react'
+import React, { ComponentType } from 'react'
 import storybookTheme from './storybookTheme'
-import { ThemeProvider } from 'styled-components/native'
-import { Provider, useDispatch, useSelector } from 'react-redux'
+import { Provider } from 'react-redux'
 import { store } from '../utils/Store'
-import { StoreTheme } from '../utils/Store/Theme/theme.types'
-import { StyleSheet, Switch, Text } from 'react-native'
-import { View } from 'react-native'
-import { setIsDarkMode } from '../utils/Store/Theme/Theme'
+import { StyleSheet, View } from 'react-native'
 import Toast from 'react-native-toast-message'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { toastConfig } from '../utils/Config/toastConfig'
+import PhoneMockup from '../utils/Components/PhoneMockup'
+import ThemeProvider from '../utils/Components/ThemeProvider'
+import Settings from '../utils/Components/Settings'
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 8,
-  },
-  text: {
-    marginRight: 8,
-    fontWeight: '600',
+    justifyContent: 'center',
+    gap: 24,
   },
 })
-
-const ThemeDecorator = ({ children }: { children: ReactNode }) => {
-  const { theme, isDarkMode } = useSelector((state: StoreTheme) => state.theme)
-  const dispatch = useDispatch()
-
-  const handleChangeIsDarkMode = (value: boolean) => {
-    dispatch(setIsDarkMode(value))
-  }
-  return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.text}>Dark mode :</Text>
-        <Switch value={isDarkMode} onValueChange={handleChangeIsDarkMode} />
-      </View>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </>
-  )
-}
 
 export const decorators = [
   (Story: ComponentType) => (
     <View>
       <Provider store={store}>
-        <ThemeDecorator>
-          <GestureHandlerRootView>
-            <Story />
-            <Toast toastConfig={toastConfig} />
-          </GestureHandlerRootView>
-        </ThemeDecorator>
+        <ThemeProvider>
+          <View style={styles.container}>
+            <PhoneMockup>
+              <GestureHandlerRootView>
+                <Story />
+                <Toast />
+              </GestureHandlerRootView>
+            </PhoneMockup>
+            <Settings />
+          </View>
+        </ThemeProvider>
       </Provider>
     </View>
   ),
