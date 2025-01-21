6898169f89f48beaf09caece054f72ecc6e9e289
Paul Thiberville <paul.thiberville@live.fr>
2025-01-21 14:19:13 +0100

    fix toasts

--- utils/Config/toastConfig.js
+++ utils/Config/toastConfig.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { TouchableOpacity, View } from 'react-native'
 import { PanGestureHandler, GestureHandlerRootView } from 'react-native-gesture-handler'
 import { useSelector } from 'react-redux'
-import styled, { css } from 'styled-components'
+import styled, { css } from 'styled-components/native'
 
 export const getDefaultBorderRadius = size => {
   return `${parseInt(size, 10) * 0.47}px`
