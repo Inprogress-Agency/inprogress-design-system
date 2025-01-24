9b62f2f076d98093fa351dec8b8bffc1e45e9fb5
Paul Thiberville <paul.thiberville@live.fr>
2025-01-24 11:51:13 +0100

    PB-1664 Illustrated arrow button

--- src/buttons/index.ts
+++ src/buttons/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button/Button'
 export { default as IconButton } from './IconButton/IconButton'
 export { default as CopyButton } from './CopyButton/CopyButton'
+export { default as IllustratedArrowButton } from './IllustratedArrowButton/IllustratedArrowButton'
