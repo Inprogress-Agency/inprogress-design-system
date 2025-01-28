2d58668449dca80eed77d2d5ae0a3f295ec77301
Paul Thiberville <paul.thiberville@live.fr>
2025-01-28 17:12:29 +0100

    PB-1685 controls text input

---
+++ src/controls/TextInput/TextInput.tsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react'
+import { Platform } from 'react-native'
+import { StyledBottomSheetTextInput, StyledTextInput } from './TextInput.styles'
+import { StoreTheme } from '../../../utils/Store/Theme/Theme.types'
+import { useSelector } from 'react-redux'
+import { TextInputProps } from './TextInput.types'
+
+const TextInput = ({ value, insideModal = false, disabled = false, ...props }: TextInputProps) => {
+  const Input = useMemo(() => {
+    if (insideModal) {
+      return StyledBottomSheetTextInput
+    }
+    return StyledTextInput
+  }, [insideModal])
+  const { theme } = useSelector((state: { theme: StoreTheme }) => state.theme)
+  return (
+    <Input
+      defaultValue={value}
+      returnKeyType="done"
+      placeholderTextColor={theme.colors.grey2}
+      editable={!disabled}
+      selectionColor={Platform.OS === 'ios' ? theme.colors.primary : undefined}
+      {...props}
+    />
+  )
+}
+
+export default TextInput
