85df99d6beb49003539a91b4ea248191bc304538
Paul Thiberville <paul.thiberville@live.fr>
2025-01-29 12:55:42 +0100

    PB-1688 controls search bar

---
+++ src/controls/SearchBar/SearchBar.styles.tsx
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components/native'
+import { StyledComponentProps } from '../../types/StyledComponent'
+import TextInput from '../TextInput/TextInput'
+import { TouchableOpacity, View } from 'react-native'
+
+export const StyledSearch = styled(View)`
+  ${({ theme }: StyledComponentProps) => css`
+    width: ${theme.size.fill};
+    padding-left: ${theme.space.std};
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background-color: ${theme.colors.grey1};
+    border-radius: ${theme.borderRadius.l};
+    min-height: ${theme.size.m};
+  `}
+`
+
+export const StyledButton = styled(TouchableOpacity)`
+  ${({ theme }: StyledComponentProps) => css`
+    background: ${theme.colors.white};
+    margin-right: ${theme.space.std};
+    border-radius: 999px;
+  `}
+`
+
+export const StyledFakeTextInput = styled(Text)`
+  ${({ theme }: StyledComponentProps) => css`
+    padding: ${theme.space.std} ${theme.space.m};
+    color: ${theme.colors.grey2};
+  `}
+`
+
+export const StyledTextInput = styled(TextInput)`
+  ${({ theme }: StyledComponentProps) => css`
+    padding: 0;
+    padding-left: ${theme.space.xs};
+  `}
+`
---
+++ src/controls/SearchBar/SearchBar.tsx
@@ -0,0 +1,58 @@
+import React, { memo } from 'react'
+import {
+  StyledButton,
+  StyledFakeTextInput,
+  StyledSearch,
+  StyledTextInput,
+} from './SearchBar.styles'
+import { LineClose, Search } from '../../icons'
+import { ButtonCloseProps, SearchBarProps } from './SearchBar.types'
+import { StoreTheme } from '../../../utils/Store/Theme/Theme.types'
+import { useSelector } from 'react-redux'
+
+export const FakeSearchBar = memo(({ placeholder }: { placeholder: string }) => {
+  return (
+    <StyledSearch>
+      <Search />
+      <StyledFakeTextInput className="pl-2 grow">{placeholder}</StyledFakeTextInput>
+    </StyledSearch>
+  )
+})
+
+const ResetButton = memo(({ value, onChange }: ButtonCloseProps) => {
+  const { theme } = useSelector((state: { theme: StoreTheme }) => state.theme)
+
+  if (!value) {
+    return null
+  }
+
+  return (
+    <StyledButton onPress={() => onChange('')}>
+      <LineClose color={theme.colors.grey2} />
+    </StyledButton>
+  )
+})
+
+const SearchBar = ({
+  placeholder,
+  value,
+  onChange,
+  autoFocus = false,
+  insideModal = false,
+}: SearchBarProps) => {
+  return (
+    <StyledSearch>
+      <Search />
+      <StyledTextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChange}
+        autoFocus={autoFocus}
+        insideModal={insideModal}
+      />
+      <ResetButton value={value} onChange={onChange} />
+    </StyledSearch>
+  )
+}
+
+export default SearchBar
