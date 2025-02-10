545fa0f79628448cf49da7708fb3055a703502ce
Paul Thiberville <paul.thiberville@live.fr>
2025-02-10 17:39:02 +0100

    fix

--- src/controls/ColorPicker/ColorPicker.types.tsx
+++ src/controls/ColorPicker/ColorPicker.types.tsx
@@ -1,17 +1,16 @@
-import { StyledComponentProps } from "../../types/StyledComponent";
-import { ReactNode } from "react";
-import { Color } from "../../types/Color";
-import { TextInputProps } from "../TextInput/TextInput.types";
+import { StyledComponentProps } from '../../types/StyledComponent'
+import { ReactNode } from 'react'
+import { Color } from '../../types/Color'
+import { TextInputProps } from '../TextInput/TextInput.types'
 
-export interface StyledViewColorProps extends StyledComponentProps{
-  editable: true,
+export interface StyledViewColorProps extends StyledComponentProps {
+  editable: true
   selectedColor: Color
 }
 
-export interface ColorPickerProps extends TextInputProps{
-  title: string,
-  editable?: boolean, 
+export interface ColorPickerProps extends TextInputProps {
+  title: string
+  editable?: boolean
   children?: ReactNode
   value: Color
 }
-
