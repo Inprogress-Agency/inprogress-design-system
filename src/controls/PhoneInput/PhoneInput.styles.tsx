d5993e3cc92342302f8c150a2ced28dff6f5272c
Paul Thiberville <paul.thiberville@live.fr>
2025-02-11 11:34:44 +0100

    fix

--- src/controls/PhoneInput/PhoneInput.styles.tsx
+++ src/controls/PhoneInput/PhoneInput.styles.tsx
@@ -2,9 +2,8 @@ import styled, { css } from 'styled-components/native'
 import { StyledComponentProps } from '../../types/StyledComponent'
 import { Text } from '../../texts'
 
-
 export const StyledText = styled(Text)`
   ${({ theme }: StyledComponentProps) => css`
-    margin-bottom: ${theme.space.std}
+    margin-bottom: ${theme.space.std};
   `}
 `
