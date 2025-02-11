519ccfbecf524b19654bf139b612fa982152cc20
Paul Thiberville <paul.thiberville@live.fr>
2025-02-11 11:34:44 +0100

    fix

--- src/controls/PhoneInput/PhoneInput.tsx
+++ src/controls/PhoneInput/PhoneInput.tsx
@@ -1,25 +1,23 @@
 import { LineArrowBottom } from '../../icons'
 import React, { useRef } from 'react'
 import { StyledText } from './PhoneInput.styles'
-// @ts-ignore
 import RNPhoneInput from 'react-native-phone-number-input'
-import {  PhoneInputProps } from './PhoneInput.types'
+import { PhoneInputProps } from './PhoneInput.types'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../types/RootState'
 import { useTranslation } from 'react-i18next'
-
+import { StyleSheet } from 'react-native'
 
 const PhoneInput = ({
   label = 'phone_input.title',
   value = { number: '', code: ['FR', '+33'] },
   onChange,
   ...props
-  
-}:PhoneInputProps ) => {
-  const phoneInput = useRef(null)
+}: PhoneInputProps) => {
+  const phoneInput = useRef<any>(null)
   const { theme } = useSelector((state: RootState) => state.theme)
   const { t } = useTranslation()
-  const styles = {
+  const styles = StyleSheet.create({
     container: {
       height: 50,
       width: '100%',
@@ -44,36 +42,36 @@ const PhoneInput = ({
       height: 18,
       fontSize: 14,
     },
-  }
-  
+  })
+
   return (
     <>
       <StyledText>{t(label)}</StyledText>
       <RNPhoneInput
-          {...props}
-          ref={phoneInput}
-          defaultCode={value.code ? value.code[0] : 'FR'}
-          defaultValue={value.number || ''}
-          layout="first"
-          onChangeText={(phoneNumber: string) => {
-            onChange({ ...value, number: phoneNumber });
-          }}
-          onChangeCountry={(country: { cca2: string; callingCode: string[] }) => {
-            onChange({
-              ...value,
-              code: [country.cca2, `+${country.callingCode[0]}`],
-            });
-          }}
-          placeholder="0612345678"
-          withDarkTheme
-          withShadow={false}
-          textInputProps={{ textAlign: 'right' }}
-          renderDropdownImage={<LineArrowBottom color={theme.colors.black} />}
-          containerStyle={styles.container}
-          textContainerStyle={styles.textContainer}
-          textInputStyle={styles.textInput}
-          codeTextStyle={styles.codeText}
-        />
+        {...props}
+        ref={phoneInput}
+        defaultCode={value.code ? value.code[0] : 'FR'}
+        defaultValue={value.number || ''}
+        layout="first"
+        onChangeText={(phoneNumber: string) => {
+          onChange({ ...value, number: phoneNumber })
+        }}
+        onChangeCountry={(country: { cca2: string; callingCode: string[] }) => {
+          onChange({
+            ...value,
+            code: [country.cca2, `+${country.callingCode[0]}`],
+          })
+        }}
+        placeholder="0612345678"
+        withDarkTheme
+        withShadow={false}
+        textInputProps={{ textAlign: 'right' }}
+        renderDropdownImage={<LineArrowBottom color={theme.colors.black} />}
+        containerStyle={styles.container}
+        textContainerStyle={styles.textContainer}
+        textInputStyle={styles.textInput}
+        codeTextStyle={styles.codeText}
+      />
     </>
   )
 }
--- src/controls/PhoneInput/PhoneInput.types.tsx
+++ src/controls/PhoneInput/PhoneInput.types.tsx
@@ -1,19 +1,10 @@
-import { StyledComponentProps } from "../../types/StyledComponent";
-
-export interface StyledText extends StyledComponentProps{
-  label: string
-}
+import { StyledComponentProps } from '../../types/StyledComponent'
 
 export interface PhoneInputProps extends StyledComponentProps {
-  label: string,
+  label: string
   value: {
-    number: string;
-    code: [string, string];
-  };
-  onChange: (val: { number: string; code: [string, string] }) => void;
-
+    number: string
+    code: [string, string]
+  }
+  onChange: (val: { number: string; code: [string, string] }) => void
 }
-
-
-
-
