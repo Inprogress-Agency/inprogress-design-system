71ae37ea428afd488fc438f9d9735aee9e1c2f3a
AkselDMP <akselprogrammation@gmail.com>
2025-02-12 11:06:40 +0100

    fix PB-1726-red-badge

--- src/others/RedBadge/RedBadge.type.tsx
+++ src/others/RedBadge/RedBadge.type.tsx
@@ -2,9 +2,14 @@ import { ReactNode } from 'react'
 
 
 export interface RedBadgeProps {
+
+  // fonction pour contrôler l'affichage du badge
+
   config: {
     useDisplayBadge?: () => boolean;
   },
+
+  // Des éléments enfants optionnels à afficher à l'intérieur du badge
 
   children?: ReactNode
 
