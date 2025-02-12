7e9f47375c5aed3bbe2f21b851179ace8b4a0ba3
AkselDMP <akselprogrammation@gmail.com>
2025-02-12 08:39:20 +0100

    fix1 pb-1726-red-badge

--- .storybook/stories/Others/RedBadge.stories.tsx
+++ .storybook/stories/Others/RedBadge.stories.tsx
@@ -1,18 +1,18 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { RedBadge } from '../../../src/others'
+import type { Meta, StoryObj } from '@storybook/react';
+import { RedBadge } from '../../../src/others';
 
 const meta: Meta<typeof RedBadge> = {
   title: 'Others/RedBadge',
   component: RedBadge,
-}
+};
 
-export default meta
+export default meta;
 
 export const Overview: StoryObj<typeof RedBadge> = {
   args: {
     config: {
-      useDisplayBadge: () => false,
+      useDisplayBadge: () => true,
     },
-    children: 'we'
+    children: 'ouho',
   },
-}
+};
